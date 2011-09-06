908217b149a631a8159780c2828bf196f9b08a7e
Eduardo Carrara de Araujo <ecarrara.araujo@gmail.com>
2011-09-06 18:14:32 -0300

    Create main project and test project structures.
    Signed-off-by: Eduardo Carrara de Araujo <ecarrara.araujo@gmail.com>

---
+++ EnvTracking/src/com/uint/envtracking/EnvTrackingActivity.java
@@ -0,0 +1,13 @@
+package com.uint.envtracking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EnvTrackingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
