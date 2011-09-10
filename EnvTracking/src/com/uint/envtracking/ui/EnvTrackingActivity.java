da4c6c793c2b4d1183ad7ff4d7c4e4a8bd47fc96
Felipe Pedroso <f.pedroso677@gmail.com>
2011-09-10 15:49:25 -0300

    First version of dashboard
    Signed-off-by: Felipe Pedroso <f.pedroso677@gmail.com>

---
+++ EnvTracking/src/com/uint/envtracking/ui/EnvTrackingActivity.java
@@ -0,0 +1,15 @@
+package com.uint.envtracking.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.uint.envtracking.R;
+
+public class EnvTrackingActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_dashboard);
+    }
+}
