b7194b478161c795971c23adb583e8ebf07aa3cb
Felipe Pedroso <f.pedroso677@gmail.com>
2011-09-10 15:49:25 -0300

    First version of dashboard
    Signed-off-by: Felipe Pedroso <f.pedroso677@gmail.com>

---
+++ EnvTracking/src/com/uint/envtracking/ui/DashboardFragment.java
@@ -0,0 +1,46 @@
+package com.uint.envtracking.ui;
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uint.envtracking.R;
+
+public class DashboardFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container);
+
+        // Attach event handlers
+        root.findViewById(R.id.home_btn_1).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                
+            }
+            
+        });
+
+
+        return root;
+    }
+}
